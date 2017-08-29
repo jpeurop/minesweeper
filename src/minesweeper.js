feb806cb100a8ed17c9b4e1171da8e1c2ddea978
jpeurop <31309941+jpeurop@users.noreply.github.com>
2017-08-29 13:14:08 +0200

    Game board arrays working

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,10 +1,14 @@
 const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
 	let board = [];
-	for (let numberOfRowsIndex = 0; numberofRowsIndex < numberOfRows.length; numberofRowsIndex++){
+	for (let numberOfRowsIndex = 0; numberOfRowsIndex < numberOfRows; numberOfRowsIndex++){
 		let row = [];
-		for (let numberOfColumnsIndex = 0; numberOfColumnsIndex < numberOfColumns.lengths; numberofRowsIndex++){
-			
+		for (let numberOfColumnsIndex = 0; numberOfColumnsIndex < numberOfColumns; numberOfColumnsIndex++){
+			row.push(' ');
 		}
+		board.push(row);
 
 	}
+	return board;
 }
+
+console.log(generatePlayerBoard(10,3));
