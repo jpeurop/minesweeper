6fdaece98f170be9346c2bf18a66dbe62b53893e
jpeurop <31309941+jpeurop@users.noreply.github.com>
2017-08-30 07:38:55 +0200

    Player and bomb board nearly finished

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -11,4 +11,33 @@ const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
 	return board;
 }
 
-console.log(generatePlayerBoard(10,3));
+const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
+	let board = [];
+	for (let numberOfRowsIndex = 0; numberOfRowsIndex < numberOfRows; numberOfRowsIndex++){
+		let row = [];
+		for (let numberOfColumnsIndex = 0; numberOfColumnsIndex < numberOfColumns; numberOfColumnsIndex++){
+			row.push(null);
+		}
+		board.push(row);
+
+	}
+
+	let numberOfBombsPlaced = 0;	
+	while (numberOfBombsPlaced < numberOfBombs){
+		let randomRowIndex = Math.floor(Math.random() * numberOfRows);
+		let randomColumnIndex = Math.floor(Math.random() * numberOfColumns);
+		board[randomRowIndex][randomColumnIndex] = 'B';
+		numberOfBombsPlaced++;
+		// Control flow to not place any bombs where bombs already exist
+	}
+	return board;	
+}
+
+const printBoard = board => {
+	console.log(board.map(row => row.join(' | ')).join('\n'));
+}
+
+let playerBoard = generatePlayerBoard(3,4);
+let bombBoard = generateBombBoard(3,4,5);
+console.log('Player Board: ' + printBoard(playerBoard));
+console.log('Bomb Board: ' + printBoard(playerBoard));
